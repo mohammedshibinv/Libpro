ba1c9a94ec3c51e46b1548c810fe5d4d9b8952d5
MOHAMMED SHIBIN V <72612100+MohdShibin@users.noreply.github.com>
2022-01-22 20:06:57 +0530

    added function to insert student data into table student in mysql database

--- src/utilities/ConnectionProvider.java
+++ src/utilities/ConnectionProvider.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package utilities;
+import java.sql.*;
 
-/**
- *
- * @author SHIBIN VERENGAL
- */
 public class ConnectionProvider {
-    
+    public static Connection getcon ()
+    {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lms","root","1234");
+            return con;
+        }
+        catch(ClassNotFoundException | SQLException e){
+            System.out.println(e);
+            return null;
+        }
+    }
 }
